e5e1b903ce025c2950a666342ca7792d25ac43d8
tkxk <107747175+tkxk@users.noreply.github.com>
2024-05-22 22:13:35 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,244 @@
 function App() {
   return (
     <>
-      <h1 className="title">SUHIAB AYMAN TAKLAS</h1>
-      <h2>ISLAM</h2>
+      <div className="header">
+        <div className="container">
+          <a className="logo" href="#">
+            TK
+          </a>
+          <div className="links">
+            <span className="icon">
+              <span />
+              <span />
+              <span />
+            </span>
+            <ul>
+              <li>
+                <a href="#services">Services</a>
+              </li>
+              <li>
+                <a href="#portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#pricing">Pricing</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* landing */}
+      <div className="landing">
+        <div className="intro-text">
+          <h1>Hello There</h1>
+          <p>We are Lone - Super Creative &amp; Minimal Agency Web Template</p>
+        </div>
+      </div>
+      {/* end landing */}
+      {/* --------------------------------------- */}
+      {/* features */}
+      <div className="features">
+        <div className="container">
+          <div className="feat">
+            <i className="fas fa-magic fa-3x" />
+            <h3>C++</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum
+              soluta pariatur quos sint omnis tenetur itaque blanditiis amet.
+              Fugiat voluptatibus perspiciatis animi incidunt ad, sit quibusdam
+              quia aliquid qui aliquam!
+            </p>
+          </div>
+          <div className="feat">
+            <i className="fas fa-gem fa-3x" />
+            <h3>Python</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum
+              soluta pariatur quos sint omnis tenetur itaque blanditiis amet.
+              Fugiat voluptatibus perspiciatis animi incidunt ad, sit quibusdam
+              quia aliquid qui aliquam!
+            </p>
+          </div>
+          <div className="feat">
+            <i className="fas fa-globe-asia fa-3x" />
+            <h3>java</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum
+              soluta pariatur quos sint omnis tenetur itaque blanditiis amet.
+              Fugiat voluptatibus perspiciatis animi incidunt ad, sit quibusdam
+              quia aliquid qui aliquam!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* end features */}
+      {/* ------------------------------*/}
+      {/* start services */}
+      <div className="services" id="services">
+        <div className="container">
+          <h2 className="special-heading">Services</h2>
+          <div className="services-content">
+            <div className="col">
+              {/* start service */}
+              <div className="srv">
+                <i className="fas fa-palette fa-2x" />
+                <div className="text">
+                  <h3>Web Desine</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                    quasi excepturi nemo. Officiis vel veniam, unde id non
+                    laborum? Ducimus molestiae ipsa modi repudiandae dicta error
+                    distinctio quia dignissimos sunt.
+                  </p>
+                </div>
+              </div>
+              <div className="srv">
+                <i className="fas fa-palette fa-2x" />
+                <div className="text">
+                  <h3>Web Desine</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                    quasi excepturi nemo. Officiis vel veniam, unde id non
+                    laborum? Ducimus molestiae ipsa modi repudiandae dicta error
+                    distinctio quia dignissimos sunt.
+                  </p>
+                </div>
+              </div>
+              {/* end service */}
+            </div>
+            <div className="col">
+              {/* start service */}
+              <div className="srv">
+                <i className="fas fa-palette fa-2x" />
+                <div className="text">
+                  <h3>Web Desine</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                    quasi excepturi nemo. Officiis vel veniam, unde id non
+                    laborum? Ducimus molestiae ipsa modi repudiandae dicta error
+                    distinctio quia dignissimos sunt.
+                  </p>
+                </div>
+              </div>
+              <div className="srv">
+                <i className="fas fa-palette fa-2x" />
+                <div className="text">
+                  <h3>Web Desine</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                    quasi excepturi nemo. Officiis vel veniam, unde id non
+                    laborum? Ducimus molestiae ipsa modi repudiandae dicta error
+                    distinctio quia dignissimos sunt.
+                  </p>
+                </div>
+              </div>
+              {/* end service */}
+            </div>
+            <div className="col">
+              <div className="image">
+                <img src="img/mountains.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* end services */}
+      <hr />
+      {/* start Portfolio */}
+      <div className="portfolio" id="portfolio">
+        <div className="container">
+          <h2 className="special-heading">Portfolio</h2>
+          <p />
+          <div className="portfolio-content">
+            <div className="card">
+              <img src="img/work3.jpg" alt="" />
+              <div className="info">
+                <h3>Project her</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src="img/work4.jpg" alt="" />
+              <div className="info">
+                <h3>Project her</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src="img/work5.jpg" alt="" />
+              <div className="info">
+                <h3>Project her</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      {/* end Portfolio */}
+      <>
+        {/* start Portfolio */}
+        <div className="about" id="about">
+          <div className="container">
+            <h2 className="special-heading">About</h2>
+            <p />
+            <div className="about-content">
+              <div className="image">
+                <img src="img/land.jpg" alt="" />
+              </div>
+              <div className="text">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Doloremque enim maiores iure, nihil iusto velit officiis,
+                  magnam ullam, sapiente odit dignissimos sunt totam perferendis
+                  accusamus consequuntur nostrum cumque sed nesciunt!
+                </p>
+                <hr />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Libero perspiciatis atque recusandae exercitationem iure
+                  numquam perferendis cupiditate nobis adipisci minima, earum
+                  repudiandae laborum, fugit sed? Explicabo maxime inventore
+                  iste doloremque!
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* end Portfolio */}
+        </div>
+        {/* start Contact  */}
+        <div className="contact">
+          <div className="container">
+            <h2 className="special-heading" id="contact">
+              Contact
+            </h2>
+            <p />
+            <div className="info">
+              <p className="label">Feel Free to drop us a line at</p>
+              <a href="mailto:sohaib.tk111@gmail.com" className="link">
+                sohaib.tk111@gmail.com
+              </a>
+              <div className="social">
+                Find Us On Social NetWorks
+                <i className="fab fa-youtube" />
+                <i className="fab fa-facebook" />
+                <i className="fab fa-twitter" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end Contact  */}
+        {/* start footer */}
+        <footer>
+          ©2023 <span>Leon</span> All right Reserved
+        </footer>
+        {/* end footer */}
+      </>
     </>
   );
 }
